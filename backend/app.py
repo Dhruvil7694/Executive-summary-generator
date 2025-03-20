40d23215a46ad387174191bb3a58bb9d233ab0b3
Dhruvil <73950907+Dhruvil7694@users.noreply.github.com>
2025-03-20 15:55:28 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -115,4 +115,4 @@ def download_results(file_id):
     )
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True, threaded=True)
